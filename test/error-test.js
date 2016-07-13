d582a0e63a6a3f64bea1f7cdbec8998b59a86d8f
sebelga <sabee77@gmail.com>
2016-07-13 18:57:10 +0200

    update error test

--- test/error-test.js
+++ test/error-test.js
@@ -2,24 +2,24 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-var DatastoolsError = require('../lib/error');
+var GstoreError = require('../lib/error');
 
 describe('Datastools Errors', () => {
     "use strict";
 
     it ('should extend Error', () => {
-        expect(DatastoolsError.prototype.name).equal('Error');
+        expect(GstoreError.prototype.name).equal('Error');
     });
 
     it('should set properties in constructor', () => {
-        var error = new DatastoolsError('Something went wrong');
+        var error = new GstoreError('Something went wrong');
 
         expect(error.message).equal('Something went wrong');
-        expect(error.name).equal('DatastoolsError');
+        expect(error.name).equal('GstoreError');
     });
 
     it('should have static errors', () => {
-        expect(DatastoolsError.ValidationError).exist;
-        expect(DatastoolsError.ValidatorError).exist;
+        expect(GstoreError.ValidationError).exist;
+        expect(GstoreError.ValidatorError).exist;
     });
 });
