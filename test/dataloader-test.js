34936677fed0e581132d7d0b1600e438ec1cd37a
sebelga <sabee77@gmail.com>
2018-01-24 06:35:21 +0100

    feat(dataloader): return "null" for entity keys not found in the Datastore

--- test/dataloader-test.js
+++ test/dataloader-test.js
@@ -17,7 +17,6 @@ describe('dataloader', () => {
 
     it('should throw an error if no datastore instance passed', () => {
         const fn = () => createDataLoader();
-
         expect(fn).throw('A Datastore instance has to be passed');
     });
 
@@ -46,6 +45,39 @@ describe('dataloader', () => {
                 expect(res[0][ds.KEY].id).equal(123);
                 expect(res[1][ds.KEY].id).equal(456);
                 expect(res[2][ds.KEY].id).equal(789);
+            });
+    });
+
+    it('should return "null" for entities not found', () => {
+        const key1 = ds.key(['User', 123]);
+        const key2 = ds.key(['User', 456]);
+        const key3 = ds.key(['User', 789]);
+        const entity = { name: 'John' };
+        entity[ds.KEY] = key2;
+
+        ds.get.resolves([[entity]]);
+
+        const loader = createDataLoader(ds);
+
+        return Promise.all([loader.load(key1), loader.load(key2), loader.load(key3)])
+            .then((res) => {
+                expect(res[0]).equal(null);
+                expect(res[1][ds.KEY].id).equal(456);
+                expect(res[2]).equal(null);
+            });
+    });
+
+    it('should bypass sort if only 1 key', () => {
+        const entity = { name: 'John' };
+        const key = ds.key(['User', 123]);
+        entity[ds.KEY] = key;
+        ds.get.resolves([[entity]]);
+
+        const loader = createDataLoader(ds);
+
+        return loader.load(key)
+            .then((res) => {
+                expect(res[ds.KEY].id).equal(123);
             });
     });
 });
