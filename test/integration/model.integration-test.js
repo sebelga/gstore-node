3dd1ba12e6561c6fae0cf2deb55abb93e1ff676e
sebelga <sabee77@gmail.com>
2018-04-03 07:09:15 +0200

    test(Model): Add integration test for Schema.read setting
    #96

--- test/integration/model.integration-test.js
+++ test/integration/model.integration-test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+
 'use strict';
 
 const Datastore = require('@google-cloud/datastore');
@@ -9,6 +11,7 @@ const ds = new Datastore({ projectId: 'gstore-integration-tests' });
 gstore.connect(ds);
 
 const { expect } = chai;
+const { Schema } = gstore;
 const {
     k1, k2, k3, k4, user1,
 } = require('./data');
@@ -19,7 +22,13 @@ const cleanUp = (cb) => {
     ds.delete(allKeys).then(cb);
 };
 
-describe.only('Integration Tests (Datastore & Memory cache)', () => {
+const addKey = (key) => {
+    if (key) {
+        allKeys.push(key);
+    }
+};
+
+describe.only('Integration Tests (Model)', () => {
     beforeEach(function integrationTest() {
         if (argv.int !== true) {
             // Skip e2e tests suite
@@ -42,4 +51,45 @@ describe.only('Integration Tests (Datastore & Memory cache)', () => {
                     expect(res2[0]).deep.equal(user1);
                 });
         }));
+
+    it('Schema.read set to false should work as expected', () => {
+        const schema = new Schema({
+            email: {
+                type: 'string',
+                validate: 'isEmail',
+                required: true,
+            },
+            password: {
+                type: 'string',
+                validate: {
+                    rule: 'isLength',
+                    args: [{ min: 8, max: undefined }],
+                },
+                required: true,
+                read: false,
+                excludeFromIndexes: true,
+            },
+            state: {
+                type: 'string',
+                default: 'requested',
+                write: false,
+                read: false,
+                excludeFromIndexes: true,
+            },
+        });
+
+        const User = gstore.model('User', schema);
+        const user = new User({ email: 'test@test.com', password: 'abcd1234' });
+
+        user.save().then((entity) => {
+            addKey(entity.entityKey);
+            const response = entity.plain();
+            expect(response.password).to.not.exist;
+            expect(response.requested).to.not.exist;
+
+            const response2 = entity.plain({ readAll: true });
+            expect(response2.password).equal('abcd1234');
+            expect(response2.state).equal('requested');
+        });
+    });
 });
