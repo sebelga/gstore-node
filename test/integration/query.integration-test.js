2eb0f3f2992ee6c19d4bc0aa5b762b2c067b1f19
sebelga <sabee77@gmail.com>
2018-04-07 13:09:05 +0200

    fix(queries): Forward options object to Datastore Query
    The query.run() "options" configuration was not forwarded to the underlying Query instance. This
    means that the { consistency: 'strong' } was not taken into account. Now the options is correctly
    forwared and the "consistency" setting passed.

---
+++ test/integration/query.integration-test.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const Datastore = require('@google-cloud/datastore');
+const chai = require('chai');
+const { argv } = require('yargs');
+const gstore = require('../../lib')();
+
+const { Schema } = gstore;
+
+const ds = new Datastore({ projectId: 'gstore-integration-tests' });
+gstore.connect(ds);
+
+const { expect } = chai;
+const {
+    k1, k2, k3, k4, entity1, entity2, entity3, entity4,
+} = require('./data');
+
+const allKeys = [k1, k2, k3, k4];
+
+const cleanUp = (cb) => {
+    ds.delete(allKeys).then(cb);
+};
+
+describe('Integration Tests (Queries)', () => {
+    let Model;
+    let query;
+
+    before(function integrationTest(done) {
+        if (argv.int !== true) {
+            this.skip();
+        }
+        gstore.models = {};
+        gstore.modelSchemas = {};
+
+        const schema = new Schema({});
+        Model = gstore.model('User', schema);
+        query = Model.query();
+
+        Model.deleteAll()
+            .then(() => (
+                // Add a few entities in the Datastore
+                ds.save([entity1, entity2, entity3, entity4])
+                    .then(() => done())
+            ));
+    });
+
+    beforeEach(function beforeEachIntTest() {
+        if (argv.int !== true) {
+            this.skip();
+        }
+    });
+
+    after(function afterAllIntTest(done) {
+        if (argv.int !== true) {
+            this.skip();
+        }
+        cleanUp(() => done());
+    });
+
+    it('should forward options to underlying Datastore.Query', () => (
+        query.run({ consistency: 'strong' })
+            .then(({ entities }) => {
+                expect(entities.length).equal(2);
+            })
+    ));
+});
