d65f7cd4ebe2a6335963250ec574091dacd731fe
sebelga <sabee77@gmail.com>
2019-09-14 17:48:32 +0200

    Convert VirtualType.ts

--- src/virtualType.ts
+++ src/virtualType.ts
@@ -1,47 +1,55 @@
-'use strict';
-
 import is from 'is';
 
+import { FunctionType, GenericObject } from './types';
+
 class VirtualType {
-    constructor(name, options) {
-        this.name = name;
-        this.getter = null;
-        this.setter = null;
-        this.options = options || {};
-    }
+  private readonly name: string;
 
-    get(fn) {
-        if (!is.fn(fn)) {
-            throw new Error('You need to pass a function to virtual get');
-        }
-        this.getter = fn;
-        return this;
-    }
+  private getter: FunctionType | null;
 
-    set(fn) {
-        if (!is.fn(fn)) {
-            throw new Error('You need to pass a function to virtual set');
-        }
-        this.setter = fn;
-        return this;
-    }
+  private setter: FunctionType | null;
 
-    applyGetters(scope) {
-        if (this.getter === null) {
-            return null;
-        }
-        const v = this.getter.call(scope);
-        scope[this.name] = v;
-        return v;
-    }
+  private options: GenericObject;
 
-    applySetters(value, scope) {
-        if (this.setter === null) {
-            return null;
-        }
-        const v = this.setter.call(scope, value);
-        return v;
+  constructor(name: string, options?: GenericObject) {
+    this.name = name;
+    this.getter = null;
+    this.setter = null;
+    this.options = options || {};
+  }
+
+  get(fn: FunctionType): VirtualType {
+    if (!is.fn(fn)) {
+      throw new Error('You need to pass a function to virtual get');
     }
+    this.getter = fn;
+    return this;
+  }
+
+  set(fn: FunctionType): VirtualType {
+    if (!is.fn(fn)) {
+      throw new Error('You need to pass a function to virtual set');
+    }
+    this.setter = fn;
+    return this;
+  }
+
+  applyGetters(scope: any): unknown {
+    if (this.getter === null) {
+      return null;
+    }
+    const v = this.getter.call(scope);
+    scope[this.name] = v;
+    return v;
+  }
+
+  applySetters(value: unknown, scope: any): unknown {
+    if (this.setter === null) {
+      return null;
+    }
+    const v = this.setter.call(scope, value);
+    return v;
+  }
 }
 
 export default VirtualType;
