19d10e03225d194bf0d070af0bcac6925a59bf7e
sebelga <sabee77@gmail.com>
2019-12-15 17:10:48 +0530

    Update coveralls script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+echo "--------------------------------"
+echo "Installing root dependencies..."
 yarn install
 
 # Install gstore-node dependencies
+echo "--------------------------------"
+echo "Installing gstore-node dependencies"
 cd $(pwd)/packages/gstore-node
 yarn install
 yarn link
 
 # Install gstore-datastore-adapter dependencies
+echo "--------------------------------"
+echo "Installing gstore-datstore-adapter dependencies"
 cd ../gstore-datastore-adapter
 yarn install
 yarn link gstore-node
