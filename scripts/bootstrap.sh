f3e0e0bfad88cafcb437711f3339a017f85bdafe
sebelga <sabee77@gmail.com>
2019-12-15 17:03:36 +0530

    Update bootstrap.sh script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,7 +5,9 @@ yarn install
 # Install gstore-node dependencies
 cd $(pwd)/packages/gstore-node
 yarn install
+yarn link
 
 # Install gstore-datastore-adapter dependencies
 cd ../gstore-datastore-adapter
 yarn install
+yarn link gstore-node
